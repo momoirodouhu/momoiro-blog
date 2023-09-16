10dcd2f1c29ea9b6d0b03b76426089a6b81a819c
momoirodouhu <momoirodouhu@gmail.com>
2023-09-16 20:23:03 +0900

    なんかちょっとそれっぽい見た目を作った

--- src/app/posts/[[...id]]/page.js
+++ src/app/posts/[[...id]]/page.js
@@ -1,7 +1,7 @@
 import parse from 'html-react-parser'
 import microcms from "@/share/microcms";
 import Header from '@/components/header';
-import "@/share/global.scss";
+import styles from "./page.module.scss"
 
 export async function generateStaticParams() {
   const data = await microcms.getAllContentIds({endpoint: "posts",fields:"title,updatedAt"})
@@ -14,11 +14,14 @@ export async function generateStaticParams() {
 export default async function FirstPost(params) {
   const ids = params.params.id
   const post = await microcms.get({endpoint: "posts",contentId:ids[0]})
+  //console.log(styles)
 
   return (
     <div>
       <Header></Header>
-      <div id="main-content">{parse(post.content)}</div>
+      <div id="main" className={styles.main}>
+        <div id="content" className={styles.content}>{parse(post.content)}</div>
+      </div>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import styles from "./header.module.scss"
 export default function header() {
     return (
         <div className={styles.header}>
-            <p>here is header</p>
+            <a>桃色Archive</a>
+            <a>Categories</a>
+            <a>About</a>
         </div>
     );
 }
