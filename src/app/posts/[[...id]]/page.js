515828997ab51ce32590583b036741faa39edf34
momoirodouhu <momoirodouhu@gmail.com>
2023-09-17 18:21:23 +0900

    インフォカラムを作成、プロフカードを追加

--- src/app/posts/[[...id]]/page.js
+++ src/app/posts/[[...id]]/page.js
@@ -1,7 +1,8 @@
 import parse from 'html-react-parser'
 import microcms from "@/share/microcms";
-import Header from '@/components/header';
-import styles from "./page.module.scss"
+import Header from '@/components/Header';
+import ProfCard from "@/components/ProfCard"
+import styles from "./post.module.scss"
 
 export async function generateStaticParams() {
   const data = await microcms.getAllContentIds({endpoint: "posts",fields:"title,updatedAt"})
@@ -20,6 +21,9 @@ export default async function FirstPost(params) {
     <div>
       <Header></Header>
       <div id="main" className={styles.main}>
+        <div className={styles.info}>
+          <ProfCard></ProfCard>
+        </div>
         <div id="content" className={styles.content}>{parse(post.content)}</div>
       </div>
     </div>
---
+++ src/components/ProfCard.js
@@ -0,0 +1,14 @@
+import styles from "./ProfCard.module.scss"
+
+export default function ProfCard(){
+    return (
+        <div className={styles.prof_card}>
+            <img src="/momoiro_avatar.png" className={styles.avatar}/>
+            <div className={styles.self_description}>
+                <p>桃色豆腐</p>
+                <p>momoirodouhu</p>
+                <p className={styles.detail}>趣味でちょこっとプログラムを書いてる一般学生<br/>ちなみに桃色要素も豆腐要素も一切ない</p>
+            </div>
+        </div>
+    )
+}
