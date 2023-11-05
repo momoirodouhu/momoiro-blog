0c924c3dba8931b931198aa91200c2b7ca4ca92c
momoirodouhu <momoirodouhu@gmail.com>
2023-11-05 19:54:37 +0900

    draftページ実装とそれに伴うoutput:"export"の無効化

---
+++ src/app/api/draft/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import microcms from "@/share/microcms";
+
+export function GET(request) {
+    const params = request.nextUrl.searchParams;
+    if(!(params.get("id") && params.get("draft_Key"))){
+        return NextResponse.json({ message: "Bad Request"}, { status: 400 })
+    }
+    console.log("draft proxy : " + params)
+    return microcms.get({endpoint: "posts",contentId:params.get("id"),queries: { draftKey: params.get("draft_key") }})
+        .then(response => {
+            return NextResponse.json(response)
+        }).catch(error =>{return NextResponse.json({ message: "Inter ServerError",error_description:error }, { status: 500 })})
+}
---
+++ src/app/posts/draft/page.js
@@ -0,0 +1,56 @@
+"use client"
+import parse from 'html-react-parser'
+import { useSearchParams } from "next/navigation";
+import { redirect } from "next/navigation";
+import { useEffect } from 'react';
+import { render } from 'react-dom';
+import Header from '@/components/Header';
+import ProfCard from "@/components/ProfCard"
+import Description from '@/components/Description';
+import global_styles from "@/share/global.module.scss"
+import styles from "./draft.module.scss"
+
+const replace = (node) => {
+    if (node.name === 'img') {
+        node.attribs["class"] = (node.attribs["class"] ?? "") + " " + styles.imgcontent
+    }
+}
+
+export default function Draft() {
+    const params = useSearchParams();
+    if (!params.get("draft_key")){redirect(params.has("id") ? "/posts/" + params.get("id") : "/")}
+    console.log(params.toString())
+    var isFirstUseEffect = true
+    useEffect(()=>{
+        if (isFirstUseEffect){
+            isFirstUseEffect = false;
+            fetch("/api/draft?"+params.toString(),{ cache: 'no-store' }).then(response =>{
+                if (response.status!=200){
+                    response.text().then(error_text => render((<p>error:{error_text}</p>),document.getElementById("main")))
+                }else{
+                    response.json().then(post => {
+                        console.log(post)
+                        const mainDom = (
+                            <>
+                                <div className={global_styles.info}>
+                                    <Description post={post}></Description>
+                                    <ProfCard />
+                                </div>
+                                <div id="content" className={styles.content}>
+                                    <div className={styles.draftwern}>ドラフトページ！！ まだ公開できてないで！</div>
+                                    {parse(post.content,{ replace })}
+                                </div>
+                            </>
+                        )
+                        render(mainDom,document.getElementById("main"))
+                }
+            )}}).catch()
+        }
+    })
+    return (
+        <div>
+            <Header />
+            <div id="main" className={global_styles.main}></div>
+        </div>
+    );
+}
