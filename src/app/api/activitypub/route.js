ce4ce330f7b7435edda12d57f0aa243258e119bc
momoirodouhu <momoirodouhu@gmail.com>
2023-11-17 00:48:55 +0900

    content-typeがjsonかチェックしてみる

--- src/app/api/activitypub/route.js
+++ src/app/api/activitypub/route.js
@@ -47,7 +47,8 @@ export function GET(request) {
 export function POST(request){
     console.log("POST:"+request.nextUrl.pathname)
     if(request.nextUrl.pathname == "/activitypub/inbox"){
+        if(req.headers['content-type'].include("json")){console.log("content-type dose not JSON. req:"+req.headers['content-type'])}
         console.log("inbox poted")
-        console.log(request)
+        console.log(JSON.parse(request.body))
     }
 }
