365c5b4c9d5bd00056f9670ede017ab5a43070ed
momoirodouhu <momoirodouhu@gmail.com>
2023-12-11 22:53:48 +0900

    ミドルウェアでnullを考慮してなかった

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
     //console.log(request.method +": "+ request.nextUrl.pathname)
-    if(request.headers.get("accept").includes("application/activity+json") ||
+    if(request.headers.get("accept")?.includes("application/activity+json") ||
     request.nextUrl.pathname.startsWith("/activitypub")){
         return NextResponse.rewrite(new URL('/api/activitypub', request.url))
     }
